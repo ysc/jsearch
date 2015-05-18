eda58da88b5d3f8f62568d9d213cb2263c5957b1
ysc <yang-shangchuan@qq.com>
2015-05-19 01:48:10 +0800

    索引接口

---
+++ src/main/java/org/apdplat/jsearch/index/Indexer.java
@@ -0,0 +1,31 @@
+/*
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.apdplat.jsearch.index;
+
+/**
+ * 索引接口
+ * @author 杨尚川
+ */
+public interface Indexer {
+    /**
+     * 为目录及其所有子目录下的所有文本建立索引
+     * @param dir 目录
+     */
+    public void indexDir(String dir);
+}
